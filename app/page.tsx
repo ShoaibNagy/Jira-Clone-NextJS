cea8b708d11d233dcd6302c5fd180d7b1084af47
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-04-05 14:13:13 +0200

    feat: add new UI components including Field, Input, Label, Popover, ScrollArea, Select, Separator, Sheet, Skeleton, Sonner, Table, Tabs, Textarea; update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button size={"sm"}>Click me</Button>
-      <p className="text-red-500 font-semibold">Antonio</p>
+    <div className="flex gap-4">
+      <Button variant={"primary"} size={"xs"}>Primary</Button>
+      <Button variant={"secondary"} size={"sm"}>Secondary</Button>
+      <Button variant={"destructive"} size={"sm"}>Destructive</Button>
+      <Button variant={"ghost"} size={"sm"}>Ghost</Button>
+      <Button variant={"muted"} size={"sm"}>Muted</Button>
+      <Button variant={"outline"} size={"sm"}>Outline</Button>
+      <Button variant={"tritary"} size={"sm"}>Tritary</Button>
     </div>
 
   );
