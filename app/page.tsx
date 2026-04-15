c9e77327f3471227b7ee1f951dca9cdff862b5cc
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-04-15 15:33:46 +0200

    feat: implement user session management with current user retrieval and logout functionality; add session middleware for authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) router.push("/sign-in");
+  }, [data])
   return (
-    <div className="flex gap-4">
-      <Button variant={"primary"} size={"xs"}>Primary</Button>
-      <Button variant={"secondary"} size={"sm"}>Secondary</Button>
-      <Button variant={"destructive"} size={"sm"}>Destructive</Button>
-      <Button variant={"ghost"} size={"sm"}>Ghost</Button>
-      <Button variant={"muted"} size={"sm"}>Muted</Button>
-      <Button variant={"outline"} size={"sm"}>Outline</Button>
-      <Button variant={"tritary"} size={"sm"}>Tritary</Button>
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
+      </Button>
     </div>
 
   );
