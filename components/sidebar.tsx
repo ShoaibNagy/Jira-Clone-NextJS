ca40ad9077448679075a6df9b35655a5a2ccfac8
Shoaib Nagy <94715337+Developer24032@users.noreply.github.com>
2026-05-05 09:50:08 +0300

    feat: implement workspace switcher component and API for fetching workspaces; update sidebar to include workspace switcher

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { DottedSeparator } from "./dotted-separator";
 import { Navigation } from "./navigation";
+import { WorkspaceSwitcher } from "./workspace-switcher";
 
 export function Sidebar() {
   return (
@@ -11,6 +12,8 @@ export function Sidebar() {
       <Link href={"/"}>
         <Image src={"/logo.svg"} alt="logo" width={164} height={48}></Image>
       </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
       <DottedSeparator className="my-4" />
       <Navigation />
     </aside>
--- features/workspaces/server/route.ts
+++ features/workspaces/server/route.ts
@@ -6,6 +6,16 @@ import { DATABESE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from '@/config';
 import { ID } from 'node-appwrite';
 
 const app = new Hono()
+  .get("/", sessionMiddleware, async (c) => {
+    const databases = c.get("databases");
+
+    const workspaces = await databases.listDocuments(
+      DATABESE_ID,
+      WORKSPACES_ID,
+    );
+
+    return c.json({ data: workspaces })
+  })
   .post(
     "/",
     zValidator("form", createWorkspaceSchema),
