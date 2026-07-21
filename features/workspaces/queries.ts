8a14810b55c851b72f7a47e1c437fcfd756131d2
Shoaib Nagy <shoaibwalid94@gmail.com>
2026-07-21 14:57:43 +0300

    feat: refactor authentication queries to use createSessionClient for session management; streamline workspace queries by removing redundant session handling

--- features/workspaces/queries.ts
+++ features/workspaces/queries.ts
@@ -1,24 +1,13 @@
-import { cookies } from "next/headers";
-import { Account, Client, Databases, Query } from "node-appwrite";
+import { Query } from "node-appwrite";
 
-import { AUTH_COOKIE } from "../auth/constants";
 import { DATABASE_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "../members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = (await cookies()).get(AUTH_COOKIE);
-
-    client.setSession(session!.value);
-    if (!session) return { documents: [], total: 0 };
-
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(
@@ -54,17 +43,7 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = (await cookies()).get(AUTH_COOKIE);
-
-    client.setSession(session!.value);
-    if (!session) return null;
-
-    const databases = new Databases(client);
-    const account = new Account(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const member = await getMember({
