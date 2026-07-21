46acd1451eb2fa012c62a62b57b08f645c215694
Shoaib Nagy <shoaibwalid94@gmail.com>
2026-07-21 21:44:48 +0300

    feat: implement workspace joining functionality with invite code; add client and server components for workspace info retrieval and error handling

---
+++ app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/client.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/useWorkspaceId";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { JoinWorkspaceForm } from "@/features/workspaces/components/JoinWorkspaceForm";
+
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+
+export const WorkspaceIdJoinClient = () => {
+  const workspaceId = useWorkspaceId();
+  const { data: initialValues, isLoading } = useGetWorkspaceInfo({ workspaceId });
+
+  if (isLoading) {
+    return <PageLoader />
+  }
+
+  if (!initialValues) {
+    return <PageError message="Project not found" />
+  }
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <JoinWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
