d624a336e02dd7f2725e74109a2da872e63c3807
Shoaib Nagy <shoaibwalid94@gmail.com>
2026-07-27 19:24:36 +0300

    feat: enhance Badge component with TaskStatus variants for improved task representation

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -3,6 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { Slot } from "radix-ui"
 
 import { cn } from "@/lib/utils"
+import { TaskStatus } from "@/features/tasks/types"
 
 const badgeVariants = cva(
   "group/badge inline-flex h-5 w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-4xl border border-transparent px-2 py-0.5 text-xs font-medium whitespace-nowrap transition-all focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&>svg]:pointer-events-none [&>svg]:size-3!",
@@ -19,6 +20,16 @@ const badgeVariants = cva(
         ghost:
           "hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50",
         link: "text-primary underline-offset-4 hover:underline",
+        [TaskStatus.TODO]: 
+          "border-transparent bg-red-400 text-primary hover:bg-red-400/80",
+        [TaskStatus.IN_PROGRESS]:
+          "border-transparent bg-yellow-400 text-primary hover:bg-yellow-400/80",
+        [TaskStatus.IN_REVIEW]:
+          "border-transparent bg-blue-400 text-primary hover:bg-blue-400/80",
+        [TaskStatus.DONE]:
+          "border-transparent bg-emerald-400 text-primary hover:bg-emerald-400/80",
+        [TaskStatus.BACKLOG]:
+          "border-transparent bg-pink-400 text-primary hover:bg-pink-400/80",
       },
     },
     defaultVariants: {
