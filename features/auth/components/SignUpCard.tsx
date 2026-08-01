63a08318e0dc768fbafa91a29e1dfd3238d553d3
Shoaib Nagy <shoaibwalid94@gmail.com>
2026-08-01 21:36:18 +0300

    refactor: enhance SignUpCard component structure and improve accessibility

--- features/auth/components/SignUpCard.tsx
+++ features/auth/components/SignUpCard.tsx
@@ -1,33 +1,35 @@
 "use client";
 
+import { z } from "zod";
+import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { signUpWithGithub, signUpWithGoogle } from "@/lib/oauth";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardHeader, 
   CardTitle
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
+
 import { registerSchema } from "../schemas";
 import { useRegister } from "../api/use-register";
 
-export default function SignUpCard() {
+export const SignUpCard = () => {
   const { mutate, isPending } = useRegister();
 
   const form = useForm<z.infer<typeof registerSchema>>({
@@ -35,8 +37,8 @@ export default function SignUpCard() {
     defaultValues: {
       name: "",
       email: "",
-      password: ""
-    }
+      password: "",
+    },
   });
 
   const onSubmit = (values: z.infer<typeof registerSchema>) => {
@@ -44,23 +46,24 @@ export default function SignUpCard() {
   };
 
   return (
-    <Card className="w-full h-full md:w-121.5 border-none shadow-none">
-      <CardHeader className="flex flex-col  items-center justify-center text-center p-7">
-        <CardTitle className="flex text-2xl">
-          Create an Account!
+    <Card className="w-full h-full md:w-121.75 border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">
+          Sign Up
         </CardTitle>
         <CardDescription>
           By signing up, you agree to our{" "}
-          <Link href={"/privacy-policy"}>
+          <Link href="/privacy">
             <span className="text-blue-700">Privacy Policy</span>
-          </Link>{" "}and{" "}
-          <Link href={"/terms-of-service"}>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
             <span className="text-blue-700">Terms of Service</span>
-          </Link>.
+          </Link>
         </CardDescription>
       </CardHeader>
-      <div className="px-7 mb-2">
-        <DottedSeparator />
+      <div className="px-7">
+        <DottedSeparator  />
       </div>
       <CardContent className="p-7">
         <Form {...form}>
@@ -90,7 +93,7 @@ export default function SignUpCard() {
                     <Input
                       {...field}
                       type="email"
-                      placeholder="Enter your email address"
+                      placeholder="Enter email address"
                     />
                   </FormControl>
                   <FormMessage />
@@ -113,7 +116,7 @@ export default function SignUpCard() {
                 </FormItem>
               )}
             />
-            <Button disabled={isPending} size={"lg"} className="w-full">
+            <Button disabled={isPending} size="lg" className="w-full">
               Register
             </Button>
           </form>
@@ -124,22 +127,24 @@ export default function SignUpCard() {
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
+          onClick={() => signUpWithGoogle()}
           disabled={isPending}
-          variant={"secondary"}
-          size={"lg"}
+          variant="secondary"
+          size="lg"
           className="w-full"
         >
           <FcGoogle className="mr-2 size-5" />
-          Register with Google
+          Login with Google
         </Button>
         <Button
+          onClick={() => signUpWithGithub()}
           disabled={isPending}
-          variant={"secondary"}
-          size={"lg"}
+          variant="secondary"
+          size="lg"
           className="w-full"
         >
           <FaGithub className="mr-2 size-5" />
-          Register with GitHub
+          Login with Github
         </Button>
       </CardContent>
       <div className="px-7">
@@ -147,14 +152,12 @@ export default function SignUpCard() {
       </div>
       <CardContent className="p-7 flex items-center justify-center">
         <p>
-          Already have an account?{" "}
-          <Link href={"/sign-in"}>
-            <span className="text-blue-700 hover:underline cursor-pointer">
-              &nbsp;Sign in
-            </span>
+          Already have an account?
+          <Link href="/sign-in">
+            <span className="text-blue-700">&nbsp;Sign In</span>
           </Link>
         </p>
       </CardContent>
     </Card>
   );
-}
+};
