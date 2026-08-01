20ca7fcac24f234b5b995e311b4baa24934a86dd
Shoaib Nagy <shoaibwalid94@gmail.com>
2026-08-01 21:36:08 +0300

    refactor: improve SignInCard component structure and styling

--- features/auth/components/SignInCard.tsx
+++ features/auth/components/SignInCard.tsx
@@ -1,40 +1,42 @@
 "use client";
 
+import { z } from "zod";
+import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
+import { signUpWithGithub, signUpWithGoogle } from "@/lib/oauth";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
+import { 
+  Card, 
+  CardContent, 
+  CardHeader, 
   CardTitle
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage
+  FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
+
 import { loginSchema } from "../schemas";
 import { useLogin } from "../api/use-login";
 
-export default function SignInCard() {
+export const SignInCard = () => {
   const { mutate, isPending } = useLogin();
-  
+
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
-      password: ""
-    }
+      password: "",
+    },
   });
 
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
@@ -42,18 +44,18 @@ export default function SignInCard() {
   };
 
   return (
-    <Card  className="w-full h-full md:w-121.5 border-none shadow-none">
+    <Card className="w-full h-full md:w-121.75 border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="flex text-2xl">
-          Welcome Back!
+        <CardTitle className="text-2xl">
+          Welcome back!
         </CardTitle>
       </CardHeader>
-      <div className="px-7 mb-2">
-        <DottedSeparator />
+      <div className="px-7">
+        <DottedSeparator  />
       </div>
       <CardContent className="p-7">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4" suppressHydrationWarning>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
               name="email"
               control={form.control}
@@ -70,7 +72,6 @@ export default function SignInCard() {
                 </FormItem>
               )}
             />
-
             <FormField
               name="password"
               control={form.control}
@@ -87,7 +88,7 @@ export default function SignInCard() {
                 </FormItem>
               )}
             />
-            <Button disabled={isPending} size={"lg"} className="w-full">
+            <Button disabled={isPending} size="lg" className="w-full">
               Login
             </Button>
           </form>
@@ -98,22 +99,24 @@ export default function SignInCard() {
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
+          onClick={() => signUpWithGoogle()}
           disabled={isPending}
-          variant={"secondary"}
-          size={"lg"}
+          variant="secondary"
+          size="lg"
           className="w-full"
         >
           <FcGoogle className="mr-2 size-5" />
           Login with Google
         </Button>
         <Button
+          onClick={() => signUpWithGithub()}
           disabled={isPending}
-          variant={"secondary"}
-          size={"lg"}
+          variant="secondary"
+          size="lg"
           className="w-full"
         >
           <FaGithub className="mr-2 size-5" />
-          Login with GitHub
+          Login with Github
         </Button>
       </CardContent>
       <div className="px-7">
@@ -122,13 +125,11 @@ export default function SignInCard() {
       <CardContent className="p-7 flex items-center justify-center">
         <p>
           Don&apos;t have an account?
-          <Link href={"/sign-up"}>
-            <span className="text-blue-700 hover:underline cursor-pointer">
-              &nbsp;Create an account
-            </span>
+          <Link href="/sign-up">
+            <span className="text-blue-700">&nbsp;Sign Up</span>
           </Link>
         </p>
       </CardContent>
     </Card>
   );
-}
+};
